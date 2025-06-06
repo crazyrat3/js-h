3f0ed73707080038086a353e7f7ca116395e33a8
crazyrat3 <theclockman0304@gmail.com>
2025-06-06 12:47:30 +0000

    js datatypes

---
+++ 01_basics/)2_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+
+//number => 2 to power 53
+//bigInt
+//boolean
+//string
+//null => standalone value
+//undefined 
+//symbol =>unique
+//object
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
